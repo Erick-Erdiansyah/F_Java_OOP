e79fd762b62d89f45f38ac93c33fb447216005aa
erick-erdiansyah <erdiansyah290@gmail.com>
2023-03-06 13:28:02 +0700

    first test flight

---
+++ test.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+// input output in java, close scanner with try catch
+// public class test {
+    //     public static void main(String[] args) {
+        //         try (Scanner Ascanner = new Scanner(System.in)) {
+            //             System.out.println("Hello World, say something :");
+            
+            //             String test = Ascanner.nextLine();
+            //             System.out.println(test);
+            //         }
+            //     }
+            // }
+            
+
+// input output in java, close scanner using close method
+public class test {
+    public static void main(String[] args) {
+        Scanner Ascanner = new Scanner(System.in);
+        System.out.println("Hello World, say something :");
+
+        String test = Ascanner.nextLine();
+        System.out.println(test + " Fuck You");
+        Ascanner.close();
+    }
+}
